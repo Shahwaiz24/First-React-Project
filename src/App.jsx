036028dab16a6de7b3d0c9eaf3eb8a2574ba9c8b
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-04 15:54:20 +0500

    First Counter With State Updating Hook With Some Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,33 @@ import {useState} from "react";
 
 function App() {
   const [value, setValue] = useState(0);
+  const [warningValue, SetwarningValue] = useState("");
 
   let AddValue = () => {
-    setValue(value +1);
-    console.log("Updated Value", value)
+    if(value != 20){
+      SetwarningValue("");
+      setValue(value +1);
+      console.log("Updated Value", value)
+    }else{
+      SetwarningValue("You Can't Increase Value After Reaching 20");
+    }
+   
   }
   let DecreaseValue = () => {
     if(value != 0){
+      SetwarningValue("");
       setValue(value -1);
       console.log("Updated Value", value)
     }
   
   }
+  console.log("Rebuild");
 
   return (
    <>
    <div>
+    <h1>{warningValue}</h1>
+    <br />
     <h1>My First Counter {value}</h1>
     <br />
     <button onClick={
