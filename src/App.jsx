0f02363eb1f51808288d281284a7cefa3b5ee566
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-04 15:39:58 +0500

    First Counter With State Updating Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-
+import {useState} from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  let AddValue = () => {
+    setValue(value +1);
+    console.log("Updated Value", value)
+  }
+  let DecreaseValue = () => {
+    if(value != 0){
+      setValue(value -1);
+      console.log("Updated Value", value)
+    }
+  
+  }
 
   return (
-   <h1>First React Project</h1>
+   <>
+   <div>
+    <h1>My First Counter {value}</h1>
+    <br />
+    <button onClick={
+AddValue
+}
+>
+      Add Value {value}
+    </button>
+<br />
+<button onClick={DecreaseValue}>
+  Decrease Value {value}
+</button>
+   </div>
+   </>
   )
 }
 
